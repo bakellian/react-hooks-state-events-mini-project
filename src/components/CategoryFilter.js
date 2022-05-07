25ce4c9d4d8686f20f13e76309c5e4a79f9cac12
Bridget Akellian <akellianbv@gmail.com>
2022-05-07 00:52:16 -0400

    completed

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,28 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, categoryState, handleCategoryState }) {
+
+  const handleCategory = (category) => {
+    handleCategoryState(category);
+  }
+
+  const categoryButtons = categories.map(category => (
+    //why does the on click below need to be an anonymous function??
+    //because we want to pass in "category" but by default an event listener will pass in an event. 
+    <button 
+        key={category} 
+        onClick={() => handleCategory(category)}
+        className={category === categoryState ? "selected" : null }
+        >
+          {category}
+        </button>
+  ))
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {/* render <button> elements for each category here */}
+      {categoryButtons}
     </div>
   );
 }
