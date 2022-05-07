52765e5a3a09a24cd8eb77f33afdde4e2413b9aa
Bridget Akellian <akellianbv@gmail.com>
2022-05-07 00:53:21 -0400

    complete

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks, handleDelete }) {
+  // transfrom array into array of JSK elements
+  const taskArray = tasks.map((task, index) => {
+    return <Task 
+      key={index} 
+      text={task.text} 
+      category={task.category}
+      handleDelete={handleDelete}
+      />
+  });
+
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {taskArray}
     </div>
   );
 }
