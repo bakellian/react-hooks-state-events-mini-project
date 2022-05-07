0b10135ec470cb30584181ef6c79331a5394f555
Bridget Akellian <akellianbv@gmail.com>
2022-05-07 00:52:58 -0400

    completed

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { TASKS } from "../data";
 
-function Task() {
+
+// pass down the props from tasklist
+function Task({ text, category, handleDelete }) {
+
+  const handleClick = () => {
+    handleDelete(text);
+  }
+
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button className="delete" onClick={handleClick}>X</button>
     </div>
   );
 }
